1f0faf76cd28ca6595972120fea4c9c2aecb77ac
RohanRamola <rohanramola63@gmail.com>
2023-08-29 03:37:59 +0530

    Form created and integrated with Api

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ import path from 'path'
 
 const app = express()
 
+//Using Middleware
+app.use(express.static(path.join(path.resolve(), 'public')))
+app.use(express.urlencoded({extended: true}))
+
+app.set('view engine', 'ejs')
+
 app.get("/", (req,res) => {
 
-    const pathName = path.resolve()
-    res.sendFile(path.join(pathName, 'index.html'))
+    // const pathName = path.resolve()
+    // res.sendFile(path.join(pathName, 'index.html'))
+
+    res.render('index', {name: 'peter'})
+})
+
+app.get("/success", (req,res) => {
+    res.render('success')
+})
+
+app.post("/submit", (req,res) => {
+    console.log(req.body)
+    res.redirect("/success")
 })
 
 app.listen(5000, () => {
